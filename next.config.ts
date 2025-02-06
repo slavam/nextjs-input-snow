98a86be258f6e9f17b5f2fbdb22862e450a73d94
slavam <mwm1955@gmail.com>
2025-02-06 13:43:51 +0300

    nextConfig changed again

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
-      allowedOrigins: ["10.54.1.12"]
+      allowedOrigins: ["10.54.1.12:8086",'31.133.32.14:8086']
     },
   }
 }  
