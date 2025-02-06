53698924afa91fbdacfbc5d2fcf4b67081f87ca2
slavam <mwm1955@gmail.com>
2025-02-06 13:24:41 +0300

    nextConfig changed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["10.54.1.12"]
+    },
+  }
+}  
+module.exports = nextConfig
